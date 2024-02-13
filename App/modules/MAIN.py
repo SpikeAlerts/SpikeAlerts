f2db500bc3714e6deccd6f42523bdf6790de709b
RwHendrickson <91440903+RwHendrickson@users.noreply.github.com>
2024-02-13 15:58:25 -0600

    Update MAIN.py
    Changed main's workflow numbering

--- App/modules/MAIN.py
+++ App/modules/MAIN.py
@@ -4,25 +4,24 @@ This function is the workflow of one regular update
 # It Sequentially calls many other workflows within the same directory
 
 # 0 = Daily Updates - Less regular System-wide updates for Sensors and POIs
-# 1a = GetSort_Spikes - Gets real-time sensors' information & begins interpretation. 
+# 1 = GetSort_Spikes - Gets real-time sensors' information & begins interpretation. 
                         Returns a pd.dataframe of this called sensors_df
-# 1b = Update_Sensor_Tables - Update last_elevated, last_seen, channel_flags, and values in "Sensors" and last_update in "Sensor Type Information"
+# 2 = Update_Sensor_Tables - Update last_elevated, last_seen, channel_flags, and values in "Sensors" and last_update in "Sensor Type Information"
 
  sensors_df column 'sensor_status' is used to inform the next steps:
 
-# 2 = Update_Alerts_Tables - Create new alerts, update ongoing alerts, and end old alerts
+# 3 = Update_Alerts_Tables - Create new alerts, update ongoing alerts, and end old alerts
  
-# 3 = Update_POI_Tables - Updates the POI tables <- if .env has POI_FORM='Something'
-# Finally,
-# 6 = Send_Messages - Sends all messages <- If .env has MESSAGING='Something'
+# 4 = Update_POIs_and_Reports - Updates the POI & reports tables
 '''
 
 # Import the modules listed above
 # from modules import Daily_Updates, GetSort_Spikes, New_Alerts, Ongoing_Alerts, Ended_Alerts, Flagged_Sensors, Send_Alerts
 from modules import Daily_Updates # 0
-from modules import GetSort_Spikes # 1.a
-from modules import Update_Sensor_Tables # 1.b
-from modules import Update_Alert_Tables # 2
+from modules import GetSort_Spikes # 1
+from modules import Update_Sensor_Tables # 2
+from modules import Update_Alert_Tables # 3
+from modules import Update_POIs_and_Reports # 4
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -44,21 +43,25 @@ def main(base_config, runtime, next_system_update):
 
     # ~~~~~~~~~~~~~~~~~~~~~
     
-    # 1a) Query APIs for current data & Sort Sensors into categories
+    # 1) Query APIs for current data & Sort Sensors into categories
 
     sensors_df, sensor_types_updated = GetSort_Spikes.workflow(runtime, base_config['TIMEZONE'])
 
     if len(sensors_df) > 0:
-        # 1b) Update our database tables "Sensors" and "Sensor Type Information"
+        # 2) Update our database tables "Sensors" and "Sensor Type Information"
 
         Update_Sensor_Tables.workflow(sensors_df, sensor_types_updated, runtime)
 
         # ~~~~~~~~~~~~~~~~~~~~~
 
-        # 2) Workflow for updating our database tables "Active Alerts" and "Archived Alerts"
+        # 3) Workflow for updating our database tables "Active Alerts" and "Archived Alerts"
 
-        Update_Alert_Tables.workflow(sensors_df, runtime)
+        prev_max_alert_id = Update_Alert_Tables.workflow(sensors_df, runtime)
+        
+        # ~~~~~~~~~~~~~~~~~~~~~
 
-    
-    
+        # 4) Workflow for updating our database tables "Points of Interest" and "Reports Archive"
+
+        #Update_POIs_and_Reports.workflow(sensors_df, prev_max_alert_id, runtime)
+
     return next_system_update
